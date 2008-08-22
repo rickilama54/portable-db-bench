4f880989c0a41d023335277ba22795fc1397f5a8
valerio.barbagallo <valerio.barbagallo@c71d934b-d652-0410-91d2-b5ef238ec21c>
2008-08-22 14:24:01 +0000

    package refactoring

---
+++ trunk/db-bench/src/main/java/com/eris4/benchdb/test/person/initializator/PersonInitializator.java
@@ -0,0 +1,34 @@
+package com.eris4.benchdb.test.person.initializator;
+
+import org.apache.log4j.Logger;
+
+import com.eris4.benchdb.core.Database;
+import com.eris4.benchdb.core.DbInitializator;
+import com.eris4.benchdb.core.NoSuitableDriverException;
+import com.eris4.benchdb.core.TestDriverException;
+import com.eris4.benchdb.test.person.domain.PersonDriver;
+
+public class PersonInitializator extends DbInitializator {
+	
+	private Logger logger = Logger.getLogger(DbInitializator.class);
+
+	@Override
+	public void init(Database database, int numberOfObjects) throws NoSuitableDriverException, TestDriverException {
+		PersonDriver personDriver = (PersonDriver) database.getSpecificDriver(PersonDriver.class);
+		logger.debug("specific driver acquired");
+		personDriver.connect();
+		logger.debug("connected to the database");
+		personDriver.init(numberOfObjects);
+		logger.debug("init done of number of objects = "+numberOfObjects);
+		personDriver.close();	
+		logger.debug("closed the connection");
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Number of Person initialized: " + getNumberOfObjects();
+	}
+
+	
+	
+}
