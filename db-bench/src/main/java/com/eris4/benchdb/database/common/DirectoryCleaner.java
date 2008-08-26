5a5307f9ed3de53871e0bb26534b3acbc78ce63b
valerio.barbagallo <valerio.barbagallo@c71d934b-d652-0410-91d2-b5ef238ec21c>
2008-08-26 13:38:34 +0000

    directory cleaner now is able to clean the directory recursively. be carefull using it!

--- db-bench/src/main/java/com/eris4/benchdb/database/common/DirectoryCleaner.java
+++ db-bench/src/main/java/com/eris4/benchdb/database/common/DirectoryCleaner.java
@@ -11,9 +11,13 @@ public class DirectoryCleaner {
 	public static void clean(String directoryName){
 		try {
 			for (File file : new File(directoryName).listFiles()) {
-				boolean b = file.delete();
-				if (b==false)
-					throw new FileBusyException("cannot delete the file");
+				if (file.isDirectory()){
+					clean(file.getAbsolutePath()) ;
+				}else {
+					boolean b = file.delete();
+					if (b==false)
+						throw new FileBusyException("cannot delete the file");
+				}				
 			}
 		} catch (NullPointerException e) {
 			logger.warn("Null Pointer Exception while deleting the db "+directoryName+". Probably the first time you run this test! Don't worry");
