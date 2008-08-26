7213053b832621fdb26d512e56f3e3496103740e
valerio.barbagallo <valerio.barbagallo@c71d934b-d652-0410-91d2-b5ef238ec21c>
2008-08-26 15:41:18 +0000

    h2 support for msisdn

--- db-bench/src/main/java/com/eris4/benchdb/database/h2/H2Database.java
+++ db-bench/src/main/java/com/eris4/benchdb/database/h2/H2Database.java
@@ -9,8 +9,10 @@ import com.eris4.benchdb.core.Database;
 import com.eris4.benchdb.core.TestDriverException;
 import com.eris4.benchdb.database.common.DirectoryCleaner;
 import com.eris4.benchdb.database.h2.account.AccountH2Driver;
+import com.eris4.benchdb.database.h2.msisdn.MsisdnH2Driver;
 import com.eris4.benchdb.database.h2.person.PersonH2Driver;
 import com.eris4.benchdb.test.account.domain.AccountDriver;
+import com.eris4.benchdb.test.msisdn.domain.MsisdnDriver;
 import com.eris4.benchdb.test.person.domain.PersonDriver;
 
 public class H2Database extends Database {
@@ -18,6 +20,7 @@ public class H2Database extends Database {
 	public H2Database(){
 		add(PersonDriver.class, PersonH2Driver.class);
 		add(AccountDriver.class, AccountH2Driver.class);
+		add(MsisdnDriver.class, MsisdnH2Driver.class);
 	}
 
 	@Override
