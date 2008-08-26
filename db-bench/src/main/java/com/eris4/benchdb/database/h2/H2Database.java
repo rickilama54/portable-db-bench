941148314082675b8bde70177f1d99a5749a8410
valerio.barbagallo <valerio.barbagallo@c71d934b-d652-0410-91d2-b5ef238ec21c>
2008-08-26 14:53:28 +0000

    h2 support for account object

--- db-bench/src/main/java/com/eris4/benchdb/database/h2/H2Database.java
+++ db-bench/src/main/java/com/eris4/benchdb/database/h2/H2Database.java
@@ -8,13 +8,16 @@ import java.sql.SQLException;
 import com.eris4.benchdb.core.Database;
 import com.eris4.benchdb.core.TestDriverException;
 import com.eris4.benchdb.database.common.DirectoryCleaner;
+import com.eris4.benchdb.database.h2.account.AccountH2Driver;
 import com.eris4.benchdb.database.h2.person.PersonH2Driver;
+import com.eris4.benchdb.test.account.domain.AccountDriver;
 import com.eris4.benchdb.test.person.domain.PersonDriver;
 
 public class H2Database extends Database {
 	
 	public H2Database(){
 		add(PersonDriver.class, PersonH2Driver.class);
+		add(AccountDriver.class, AccountH2Driver.class);
 	}
 
 	@Override
