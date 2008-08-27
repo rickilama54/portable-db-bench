79b45ed702782e170ca1ab317904ed51f1539f8c
valerio.barbagallo <valerio.barbagallo@c71d934b-d652-0410-91d2-b5ef238ec21c>
2008-08-27 08:54:17 +0000

    added hsqldb database, and hsqdb support for: session, account, msisdn and person. added hsqldb to pom.xml.

---
+++ db-bench/src/main/java/com/eris4/benchdb/database/hsqldb/HsqldbDatabase.java
@@ -0,0 +1,44 @@
+package com.eris4.benchdb.database.hsqldb;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
+import com.eris4.benchdb.core.Database;
+import com.eris4.benchdb.core.TestDriverException;
+import com.eris4.benchdb.database.common.DirectoryCleaner;
+import com.eris4.benchdb.database.hsqldb.account.AccountHsqldbDriver;
+import com.eris4.benchdb.database.hsqldb.msisdn.MsisdnHsqldbDriver;
+import com.eris4.benchdb.database.hsqldb.person.PersonHsqldbDriver;
+import com.eris4.benchdb.database.hsqldb.session.SessionHsqldbDriver;
+import com.eris4.benchdb.test.account.domain.AccountDriver;
+import com.eris4.benchdb.test.msisdn.domain.MsisdnDriver;
+import com.eris4.benchdb.test.person.domain.PersonDriver;
+import com.eris4.benchdb.test.session.domain.SessionDriver;
+
+public class HsqldbDatabase extends Database {
+	
+	public HsqldbDatabase(){
+		add(PersonDriver.class, PersonHsqldbDriver.class);
+		add(AccountDriver.class, AccountHsqldbDriver.class);
+		add(MsisdnDriver.class, MsisdnHsqldbDriver.class);
+		add(SessionDriver.class, SessionHsqldbDriver.class);
+	}
+
+	@Override
+	public void clear() {
+		DirectoryCleaner.clean(getFileName());
+	}
+
+	@Override
+	public String getFileName() {
+		return "db/hsqldb";
+	}
+
+	@Override
+	public void shutdown() throws TestDriverException, FileNotFoundException,SQLException, IOException, URISyntaxException {
+		//TODO		
+	}
+
+}
