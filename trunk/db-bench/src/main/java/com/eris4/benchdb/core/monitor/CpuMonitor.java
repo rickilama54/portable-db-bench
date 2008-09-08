61c8e67e46d05bb44db1b3db418f27b468568887
valerio.barbagallo <valerio.barbagallo@c71d934b-d652-0410-91d2-b5ef238ec21c>
2008-09-08 08:15:46 +0000

    cpu monitor

---
+++ trunk/db-bench/src/main/java/com/eris4/benchdb/core/monitor/CpuMonitor.java
@@ -0,0 +1,64 @@
+package com.eris4.benchdb.core.monitor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.eris4.benchdb.core.util.ThreadUtils;
+
+public class CpuMonitor extends Monitor {
+	
+	private static final int PERIOD_CHECK = 300;
+	private boolean stop;
+	private List<ThreadMonitor> threadMonitors = new LinkedList<ThreadMonitor>();
+	private int cpuUsage;
+
+	@Override
+	public void begin() {
+		stop = false;
+		new Thread(){
+
+			public void run() {
+				int tmp;
+				while (!stop) {
+					tmp = 0;
+					for (ThreadMonitor threadMonitor : threadMonitors) {
+						tmp += threadMonitor.getValue();
+					}
+					synchronized (this) {
+						cpuUsage = tmp;
+					}
+					ThreadUtils.sleep(PERIOD_CHECK);
+				}
+			}
+		}.start();
+	}
+	
+	public void addThread(Thread thread){
+		threadMonitors.add(new ThreadMonitor(thread));		
+	}
+
+	@Override
+	public void end() {
+		stop = true;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Cpu usage";
+	}
+
+	@Override
+	public long getValue() {
+		synchronized (this) {
+			return cpuUsage;
+		}
+	}
+
+	@Override
+	public void reset() {
+		threadMonitors = new LinkedList<ThreadMonitor>();
+		stop = false;
+		cpuUsage = 0;
+	}
+
+}
